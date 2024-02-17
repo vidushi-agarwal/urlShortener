05cfb9e259bbfa3c1738f83530061d7488f07d16
vidushi-agarwal <vidushiagarwal1997@gmail.com>
2024-02-17 14:00:12 +0530

    updated url shortner logic

--- src/main/java/NumeredUrlShorteningStrategy.java
+++ src/main/java/NumeredUrlShorteningStrategy.java
@@ -1,11 +1,12 @@
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NumeredUrlShorteningStrategy implements URLShortenerStrategy{
 
     private static final String SHORT_URL_PREFIX = "https://short.url/";
-    private AtomicInteger counter = new AtomicInteger(1);
+    private Random random = new Random();
     @Override
     public String shortenUrl(String longUrl) {
-        return SHORT_URL_PREFIX + counter.incrementAndGet();
+        return SHORT_URL_PREFIX +random.nextInt(100);
     }
 }
